61c736f1c54e4c57114c96f259c3525da25c4868
Andrei <sskroi200ok@gmail.com>
2024-12-15 02:12:01 +0300

    add auth check on start with loading spinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.scss";
 import "./colors.css";
 import AppRouter from "./components/AppRouter.js";
 import Header from "./components/header/Header.js";
+import { Context } from "./index.js";
+import { checkAuth } from "./http/authAPI.js";
+import { Spinner } from "react-bootstrap";
 
 function App() {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkAuth()
+      .then(() => {
+        user.setAuth(true);
+      })
+      .catch(() => {})
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100vh",
+          alignItems: "center",
+        }}
+      >
+        <Spinner variant="light" animation="border" />
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <Header />
