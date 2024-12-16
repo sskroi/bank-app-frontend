ca11feb6cbce260d03a1dbf4c35d6b328b7f459d
Andrei <sskroi200ok@gmail.com>
2024-12-17 00:52:20 +0300

    implement account closing, update account card, add auto account list updating after action

--- src/components/AccountList.jsx
+++ src/components/AccountList.jsx
@@ -1,62 +1,148 @@
 import React, { useContext, useEffect, useState } from "react";
 import styles from "./AccountList.module.scss";
-import { getAccounts } from "../http/accountsAPI";
+import { closeAccount } from "../http/accountsAPI";
 import { Context } from "../index.js";
 import { observer } from "mobx-react-lite";
 import PropTypes from "prop-types";
-import { propTypes } from "react-bootstrap/esm/Image.js";
+import Button1 from "./UI/buttons/Button1.jsx";
+import ModalWindow2 from "./UI/ModalWindow2.jsx";
+import Input1 from "./UI/inputs/Input1.jsx";
 import { Spinner } from "react-bootstrap";
 
-const AccountList = observer(() => {
+const AccountList = observer(({ updateAccountList }) => {
   const { accounts } = useContext(Context);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getAccounts(0, 100)
-      .then((data) => accounts.setAccounts(data))
-      .catch((e) => {
-        if (e.response?.data?.message) {
-          alert(e.response.data.message);
-        } else {
-          alert(e.message);
-        }
-      })
-      .finally(() => setLoading(false));
-  }, []);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => updateAccountList(setLoading), []);
+
+  const [closingAcc, setClosingAcc] = useState(null);
+  const [transferAcc, setTransferAcc] = useState(null);
 
   return (
     <div className={styles.accountList}>
+      {closingAcc && (
+        <ModalWindow2 onClose={() => setClosingAcc(null)}>
+          <CloseAccountMenu
+            {...{ closingAcc, setClosingAcc, updateAccountList }}
+          />
+        </ModalWindow2>
+      )}
+
+      {transferAcc && (
+        <ModalWindow2 onClose={() => setTransferAcc(null)}>
+          <TransferMenu
+            {...{ transferAcc, setTransferAcc, updateAccountList }}
+          />
+        </ModalWindow2>
+      )}
+
       {loading ? (
         <Spinner />
       ) : accounts.accounts.length === 0 ? (
-        <h3>У вас нет счетов</h3>
+        <h3>У вас нет открытых счетов.</h3>
       ) : (
-        accounts.accounts.map((x) => <AccountCard key={x.number} {...x} />)
+        accounts.accounts
+          .filter((x) => !x.isClose)
+          .map((x) => (
+            <AccountCard
+              onCloseAccount={setClosingAcc}
+              onTransfer={setTransferAcc}
+              key={x.number}
+              account={x}
+            />
+          ))
       )}
     </div>
   );
 });
+AccountList.propTypes = {
+  updateAccountList: PropTypes.func,
+};
 
-const AccountCard = ({ number, balance, currency, isClose }) => {
+const AccountCard = ({
+  account,
+  account: { number, balance, currency },
+  onCloseAccount,
+  onTransfer,
+}) => {
   return (
     <div className={styles.accountCard}>
-      <div>Номер счёта:</div>
       <div>
         <b>{number}</b>
       </div>
-      <div>Баланс:</div>
       <div>
         <b>{balance}</b> {currency.toUpperCase()}
+      </div>
+      <div className={styles.accountCardButtonsCont}>
+        <Button1 onClick={() => onTransfer(account)}>Перевести</Button1>
+        <Button1>Пополнить</Button1>
+        <Button1 onClick={() => onCloseAccount(account)}>Закрыть счёт</Button1>
       </div>
     </div>
   );
 };
-
 AccountCard.propTypes = {
-  number: PropTypes.string,
-  balance: PropTypes.number,
-  currency: PropTypes.currency,
-  isClose: propTypes.bool,
+  account: PropTypes.object,
+  onCloseAccount: PropTypes.func,
+  onTransfer: PropTypes.func,
+};
+
+const TransferMenu = ({ transferAcc, setTransferAcc, updateAccountList }) => {
+  return (
+    <div className={styles.transferWindow}>
+      <h5>Перевод средств</h5>
+      <div>
+        <label>Со счёта:</label>
+        <br />
+        <b>{transferAcc.number}</b>
+        <br />
+        <b>
+          {transferAcc.balance} {transferAcc.currency.toUpperCase()}
+        </b>
+      </div>
+      <div className={styles.transferDestAccInputCont}>
+        <p>Введите счёт получателя:</p>
+        <Input1 placeholder="f47971af-0487-4fae-898d-fb9108da4dff" />
+      </div>
+      <Button1>Перевести</Button1>
+    </div>
+  );
+};
+TransferMenu.propTypes = {
+  transferAcc: PropTypes.object,
+  setTransferAcc: PropTypes.func,
+  updateAccountList: PropTypes.func,
+};
+
+const CloseAccountMenu = ({ closingAcc, setClosingAcc, updateAccountList }) => {
+  const closeAcc = async () => {
+    try {
+      await closeAccount(closingAcc);
+      updateAccountList();
+    } catch (e) {
+      if (e.response?.data?.message) {
+        alert(e.response.data.message);
+      } else {
+        alert(e.message);
+      }
+    } finally {
+      setClosingAcc(null);
+    }
+  };
+
+  return (
+    <div className={styles.closeAccountWindow}>
+      <h5>Закрытие счёта</h5>
+      <p>Вы собираетесь закрыть счёт с номером:</p>
+      <b>{closingAcc.number}</b>
+      <Button1 onClick={closeAcc}>Закрыть счёт</Button1>
+    </div>
+  );
+};
+CloseAccountMenu.propTypes = {
+  closingAcc: PropTypes.object,
+  setClosingAcc: PropTypes.func,
+  updateAccountList: PropTypes.func,
 };
 
 export default AccountList;
--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,27 +1,43 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Button1 from "../components/UI/buttons/Button1";
 import styles from "./Accounts.module.scss";
 import ModalWindow1 from "../components/UI/ModalWindow1";
 import { Dropdown } from "react-bootstrap";
-import { createAccount } from "../http/accountsAPI";
+import { createAccount, getAccounts } from "../http/accountsAPI";
 import AccountList from "../components/AccountList";
+import { Context } from "../index.js";
+import PropTypes from "prop-types";
 
 const Accounts = () => {
+  const { accounts } = useContext(Context);
+
+  const updateAccountList = (setLoading) => {
+    getAccounts(0, 100)
+      .then((data) => accounts.setAccounts(data))
+      .catch((e) => {
+        if (e.response?.data?.message) {
+          alert(e.response.data.message);
+        } else {
+          alert(e.message);
+        }
+      })
+      .finally(() => typeof setLoading === "function" && setLoading(false));
+  };
+
   return (
     <div className={styles.accountsPage}>
       <div className={styles.accountPageContent}>
         <div className={styles.btnsCont}>
-          <OpenAccBtnAndMenu />
-          <CloseAccBtnAndMenu />
+          <OpenAccBtnAndMenu updateAccountList={updateAccountList} />
         </div>
 
-        <AccountList />
+        <AccountList updateAccountList={updateAccountList} />
       </div>
     </div>
   );
 };
 
-const OpenAccBtnAndMenu = () => {
+const OpenAccBtnAndMenu = ({ updateAccountList }) => {
   const [openAccActive, setOpenAccActive] = useState(false);
 
   const [newAccCurrency, setNewAccCurrency] = useState("Выберите валюту");
@@ -42,6 +58,7 @@ const OpenAccBtnAndMenu = () => {
     try {
       const newAccNum = await createAccount(newAccCurrency.toLowerCase());
       setOpenAccActive(false);
+      updateAccountList();
       alert(`Счёт успешно создан. Номер - ${newAccNum}`);
     } catch (e) {
       if (e.response?.data?.message) {
@@ -84,22 +101,8 @@ const OpenAccBtnAndMenu = () => {
   );
 };
 
-const CloseAccBtnAndMenu = () => {
-  const [closeAccActive, setCloseAccActive] = useState(false);
-
-  return (
-    <div>
-      <Button1
-        style={{ fontSize: "20px" }}
-        onClick={() => setCloseAccActive(true)}
-      >
-        Закрыть счёт
-      </Button1>
-      <ModalWindow1 isActive={closeAccActive} setActive={setCloseAccActive}>
-        <div style={{ color: "red" }}>NOT IMPLEMENTED</div>
-      </ModalWindow1>
-    </div>
-  );
+OpenAccBtnAndMenu.propTypes = {
+  updateAccountList: PropTypes.func,
 };
 
 export default Accounts;
