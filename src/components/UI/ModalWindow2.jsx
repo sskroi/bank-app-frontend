22324ff325de243249a1c266c19da20f9c1d826d
Andrei <sskroi200ok@gmail.com>
2024-12-17 14:12:17 +0300

    add ModalWindow2 closing on esc

--- src/components/UI/ModalWindow2.jsx
+++ src/components/UI/ModalWindow2.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./ModalWindow1.module.scss";
 import PropTypes from "prop-types";
 
 export default function ModalWindow2({ onClose, children }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
   return (
     <div
       className={styles.modal + " " + styles.active}
