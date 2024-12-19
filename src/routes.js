a0c3d7681b197b99ca988dfbb86309847759500c
Andrei <sskroi200ok@gmail.com>
2024-12-19 22:33:34 +0300

    addition for prev commit

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,27 @@
 import Accounts from "./pages/Accounts";
 import Auth from "./pages/Auth";
-import { ACCOUNTS_ROUTE, SIGN_IN_ROUTE, SIGN_UP_ROUTE } from "./utils/consts";
+import Profile from "./pages/Profile";
+import TransactionsHistory from "./pages/TransactionsHistory";
+import {
+  ACCOUNTS_ROUTE,
+  HISTORY_ROUTE,
+  PROFILE_ROUTE,
+  SIGN_IN_ROUTE,
+  SIGN_UP_ROUTE,
+} from "./utils/consts";
 
 export const authRotes = [
   {
     path: ACCOUNTS_ROUTE,
     Component: Accounts,
+  },
+  {
+    path: HISTORY_ROUTE,
+    Component: TransactionsHistory,
+  },
+  {
+    path: PROFILE_ROUTE,
+    Component: Profile,
   },
 ];
 
