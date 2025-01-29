c5131ff843dfe2b7e74ed060922f1f8230bfe320
Andrei <sskroi200ok@gmail.com>
2025-01-30 01:46:48 +0300

    migrate SignInForm to typescript + refactor

--- src/components/authForms/SignInForm.tsx
+++ src/components/authForms/SignInForm.tsx
@@ -5,36 +5,42 @@ import styles from "./authForms.module.scss";
 import Button1 from "../UI/buttons/Button1";
 import { SIGN_UP_ROUTE } from "../../utils/consts";
 import Input1WithLabel from "../UI/inputs/Input1WithLabel";
-import { StoreContext } from "../../main";
 import { signIn } from "../../api/authAPI";
 import useStore from "../../hooks/useStore";
+import axios from "axios";
 
-function SignInForm() {
+const SignInForm = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-
+  const [errMsg, setErrMsg] = useState<string | null>(null);
   const navigate = useNavigate();
 
   const { user } = useStore();
-  const submit = async (e) => {
+  const onSubmit = async (e: React.MouseEvent) => {
     e.preventDefault();
+    setErrMsg(null);
     try {
-      const data = await signIn(formData.email, formData.password);
-      console.log(data);
+      await signIn(formData.email, formData.password);
       user.setAuth(true);
-    } catch (e) {
-      if (e.response?.data?.message) {
-        alert(e.response.data.message);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        if (err.response?.status == 401)
+          setErrMsg("Логин или пароль не совпадают");
+        else if (err.response?.status == 400)
+          setErrMsg("Некорректный формат данных");
+        else {
+          setErrMsg("Что-то пошло не так");
+        }
       } else {
-        alert(e.message);
+        console.error(err);
       }
     }
   };
 
   return (
-    <form onSubmit={submit} className={styles.loginFormCont}>
+    <form className={styles.loginFormCont}>
       <h2>Вход в аккаунт</h2>
 
       <div className={styles.inputFieldsCont}>
@@ -55,15 +61,28 @@ function SignInForm() {
         />
       </div>
 
+      {errMsg && <p style={{ color: "var(--error-color)" }}>{errMsg}</p>}
+
       <div className={styles.buttonsCont}>
-        <Button1 type="submit" style={{ width: "45%" }}>
+        <Button1
+          onClick={onSubmit}
+          style={{ width: "45%" }}
+          disabled={formData.email === "" || formData.password.length < 8}
+        >
           Войти
         </Button1>
 
-        <Button1 onClick={() => navigate(SIGN_UP_ROUTE)}>Регистрация</Button1>
+        <Button1
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(SIGN_UP_ROUTE);
+          }}
+        >
+          Регистрация
+        </Button1>
       </div>
     </form>
   );
-}
+};
 
 export default SignInForm;
